7ae92da7bfed42cf5891fb5b97976697548295d5
huy.nguyen <huy.nguyen@centralesupelec.fr>
2025-01-08 16:44:25 +0100

    copyright

--- mnist_conv/utils.py
+++ mnist_conv/utils.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # MIT License
 
-# Copyright (c) 2025 Jeremy Fix, Xuan-Huy Nguyen
+# Copyright (c) 2024-2025 Jeremy Fix, Xuan-Huy Nguyen
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
